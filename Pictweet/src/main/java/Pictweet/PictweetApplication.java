29351ae2c4539df50087437e5a8d9c5dd64e40d8
wataru5354 <setsunan76253@gmail.com>
2023-12-21 20:12:31 +0900

    first commit

---
+++ Pictweet/src/main/java/Pictweet/PictweetApplication.java
@@ -0,0 +1,13 @@
+package Pictweet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictweetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PictweetApplication.class, args);
+	}
+
+}
