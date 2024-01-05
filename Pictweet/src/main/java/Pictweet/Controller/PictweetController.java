34c824d69c6b623e3a6c028627f30fd64c5a5041
wataru5354 <setsunan76253@gmail.com>
2024-01-05 11:28:52 +0900

    トップページの遷移条件の変更

--- Pictweet/src/main/java/Pictweet/Controller/PictweetController.java
+++ Pictweet/src/main/java/Pictweet/Controller/PictweetController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import Pictweet.Entity.TweetEntity;
 import Pictweet.Entity.UserEntity;
@@ -17,26 +18,32 @@ import Pictweet.Form.TweetForm;
 import Pictweet.Form.UserRegistrationForm;
 import Pictweet.Service.TweetService;
 import Pictweet.Service.UserService;
+import jakarta.servlet.http.HttpSession;
 
 @Controller
+@SessionAttributes("LoginForm")
 public class PictweetController {
-	
+
 	@Autowired
 	UserService userService;
 	@Autowired
 	TweetService tweetService;
-	
-	
+	@Autowired
+	HttpSession session;
+
 	@GetMapping("/")
-	public String loginPage() {
-		return "login";
+	public String TopPage(Model model) {
+		UserEntity loginUser =(UserEntity)session.getAttribute("loginUser");
+		if(loginUser != null) {
+			List<TweetEntity> tweets = tweetService.findAll();
+			model.addAttribute("tweets", tweets);
+			session.setAttribute("loginUser", loginUser);
+			model.addAttribute("loginUser",loginUser);
+			return "index";
+		}
+			return "login";
 	}
-	
-	@GetMapping("/index")
-	public String index() {
-		return "index";
-	}
-	
+
 	/**
 	 * 新規登録画面への遷移
 	 * @return
@@ -45,30 +52,31 @@ public class PictweetController {
 	public String registrationPage() {
 		return "registration";
 	}
+
 	/*
 	 * 新規投稿画面への遷移
 	 */
 	@GetMapping("tweet/{userId}")
-	public String newTweetPage(@PathVariable("userId") Integer userId, 
+	public String newTweetPage(@PathVariable("userId") Integer userId,
 			Model model) {
 		UserEntity loginUser = userService.findByUserId(userId);
-		model.addAttribute("loginUser",loginUser);
+		model.addAttribute("loginUser", loginUser);
 		return "new";
 	}
-	
+
 	/*
 	 * 編集画面への遷移
 	 */
 	@GetMapping("/edit/{id}")
 	public String edit(@PathVariable("id") Integer id,
 			Model model) {
-		TweetEntity findTweet =tweetService.findTweet(id);
+		TweetEntity findTweet = tweetService.findTweet(id);
 		UserEntity tweetUser = findTweet.getUser();
-		model.addAttribute("findTweet",findTweet);
-		model.addAttribute("tweetUser",tweetUser);
+		model.addAttribute("findTweet", findTweet);
+		model.addAttribute("tweetUser", tweetUser);
 		return "edit";
 	}
-	
+
 	/*
 	 * 詳細画面への遷移処理
 	 */
@@ -76,17 +84,16 @@ public class PictweetController {
 	public String show(@PathVariable("id") Integer id,
 			Model model) {
 		TweetEntity showTweet = tweetService.findTweet(id);
-		model.addAttribute("showTweet",showTweet);
+		model.addAttribute("showTweet", showTweet);
 		return "show";
 	}
-	
+
 	@GetMapping("/delete/{id}")
 	public String deleteTweet(@PathVariable("id") Integer id) {
 		tweetService.deleteTweet(id);
 		return "destroy";
 	}
-	
-	
+
 	@PostMapping("userRegistration")
 	/**
 	 * 新規登録の処理
@@ -99,46 +106,47 @@ public class PictweetController {
 		String password = userRegistrationForm.getPassword();
 		String passwordConfirmation = userRegistrationForm.getPasswordConfirmation();
 		String message = "";
-		if(password.equals(passwordConfirmation)) {
+		if (password.equals(passwordConfirmation)) {
 			userService.createUser(userRegistrationForm);
 			message = "登録に成功しました。";
-			model.addAttribute("message",message);
+			model.addAttribute("message", message);
 			return "login";
 		} else {
 			message = "登録に失敗しました。";
-			model.addAttribute("message",message);
+			model.addAttribute("message", message);
 			return "registration";
 		}
 	}
-	
+
 	/*
 	 * ログイン制御
 	 */
 	@PostMapping("userLogin")
 	public String userLogin(@ModelAttribute LoginForm loginForm,
 			Model model) {
-		UserEntity loginUser =  userService.loginUser(loginForm);
+		UserEntity loginUser = userService.loginUser(loginForm);
 		String message = "";
-		if(loginUser != null) {
-			model.addAttribute("loginUser",loginUser);
+		if (loginUser != null) {
+			session.setAttribute("loginUser", loginUser);
+			model.addAttribute("loginUser", loginUser);
 			List<TweetEntity> tweets = tweetService.findAll();
-			model.addAttribute("tweets",tweets);
+			model.addAttribute("tweets", tweets);
 			return "index";
-		}else {
+		} else {
 			message = "ログインに失敗しました。";
-			model.addAttribute("message",message);
+			model.addAttribute("message", message);
 			return "login";
 		}
-		
+
 	}
-	
+
 	//新規投稿
 	@PostMapping("tweet/newTweet")
 	public String newTweet(@ModelAttribute TweetForm tweetForm) {
 		tweetService.createTweet(tweetForm);
 		return "/create";
 	}
-	
+
 	//編集
 	@PostMapping("edit/editTweet")
 	public String editTweet(@ModelAttribute TweetForm tweetForm) {
